a7e11db55c22c79ad9f0262ceace078fee80894d
mayconbalves <mayconmustaine@hotmail.com>
2021-03-21 11:12:35 -0300

    refactor(main): use server

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-console.log('main ts')
+import { Server } from './server/server'
+
+const server = new Server()
+
+server.bootstrap()
+  .then((server) => {
+    console.log('Server is lintening on:', server.application.address())
+  })
+  .catch((error) => {
+    console.log('Error failed to start')
+    console.error(error)
+    process.exit(1)
+  })
