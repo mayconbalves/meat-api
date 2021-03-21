70b7f18d684df1a0d194614a53f193ca999d4b12
mayconbalves <mayconmustaine@hotmail.com>
2021-03-21 11:09:35 -0300

    feat(server): create simplify server

---
+++ server/server.ts
@@ -0,0 +1,41 @@
+import * as restify from 'restify'
+import { environment } from '../common/environment'
+
+export class Server {
+
+  application: restify.Server
+
+  initRoutes(): Promise<any> {
+    return new Promise((resolve, rejetc) => {
+      try {
+          this.application = restify.createServer({
+          name: 'meat-api',
+          version: '1.0.0'
+        })
+
+        this.application.use(restify.plugins.queryParser())
+
+        this.application.get('/info', (req, res, next) => {
+          res.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query
+          })
+        
+          return next()
+        })
+
+        this.application.listen(environment.server.port, () => {
+          resolve(this.application)
+        })
+      } catch(error) {
+        rejetc(error)
+      }
+    })
+  }
+  bootstrap(): Promise<Server> {
+    return this.initRoutes().then(() => this)
+  }
+}
