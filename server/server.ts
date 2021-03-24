8aa8d93071f6df802d393dadb7c17a417a3f712f
mayconbalves <mayconmustaine@hotmail.com>
2021-03-24 15:47:00 -0300

    feat(server): add body parser plugin

--- server/server.ts
+++ server/server.ts
@@ -22,6 +22,7 @@ export class Server {
         })
 
         this.application.use(restify.plugins.queryParser())
+        this.application.use(restify.plugins.bodyParser())
 
         for (let router of routers) {
           router.applyRoutes(this.application)
