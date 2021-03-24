45bdd7adb1c174b14552d24c1f850a6436aebad8
mayconbalves <mayconmustaine@hotmail.com>
2021-03-24 15:47:37 -0300

    feat(users): implements method post

--- users/users.routers.ts
+++ users/users.routers.ts
@@ -24,6 +24,16 @@ class UsersRoutes extends Router {
           return next()
         })
     })
+
+    application.post('/users', (req, res, next) => {
+      let user = new User(req.body)
+      user.save()
+        .then((user) => {
+          user.password = undefined
+          res.json(user)
+          return next()
+        })
+    })
   }
 }
 
