dc872e61cec06c84ba859e1fdafcea8b79629a3f
mayconbalves <mayconmustaine@hotmail.com>
2021-03-24 15:10:36 -0300

    feat(common): create environment fro db

--- common/environment.ts
+++ common/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  server: {
-    port: process.env.SERVER_PORT || 3001
-  }
+  server: { port: process.env.SERVER_PORT || 3001 },
+  db: { url: process.env.DB_URL || 'mongodb://localhost/meat-api' }
 }
