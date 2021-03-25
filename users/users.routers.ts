7cbf5ee8e9cc683e022d861ef6e91b589a9a3b4d
mayconbalves <mayconmustaine@hotmail.com>
2021-03-25 15:49:06 -0300

    refactor(users): use method render and emit event beforeRender

--- users/users.routers.ts
+++ users/users.routers.ts
@@ -3,36 +3,31 @@ import { User } from '../users/users.model'
 import * as restify from 'restify'
 
 class UsersRoutes extends Router {
+
+  constructor() {
+    super()
+
+    this.on('beforeRender', document => {
+      document.password = undefined
+    })
+  }
+
+
   applyRoutes(application: restify.Server) {
     application.get('/users', (req, res, next) => {
       User.find()
-        .then((users) => {
-          res.json(users)
-          return next()
-        })
+        .then(this.render(res, next))
     })
 
     application.get('users/:id', (req, res, next) => {
       User.findById(req.params.id)
-        .then((user) => {
-          if(user) {
-            res.json(user)
-            return next()
-          }
-
-          res.send(404)
-          return next()
-        })
+        .then(this.render(res, next))
     })
 
     application.post('/users', (req, res, next) => {
       let user = new User(req.body)
       user.save()
-        .then((user) => {
-          user.password = undefined
-          res.json(user)
-          return next()
-        })
+        .then(this.render(res, next))
     })
 
     application.put('/users/:id', (req, res, next) => {
@@ -46,24 +41,13 @@ class UsersRoutes extends Router {
             return res.send(404)
           }
         })
-        .then((user) => {
-          res.json(user)
-          return next()
-        })
+        .then(this.render(res, next))
     })
 
     application.patch('/users/:id', (req, res, next) => {
       const options = { new: true }
       User.findByIdAndUpdate(req.params.id, req.body, options)
-        .then((user) => {
-          if(user) {
-            res.json(user)
-            return next()
-          }
-
-          res.send(404)
-          return next()
-        })
+        .then(this.render(res, next))
     })
 
     application.del('/users/:id', (req, res, next) => {
