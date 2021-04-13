7540e4f99c9658d568402317f1891a8d51765468
mayconbalves <mayconmustaine@hotmail.com>
2021-04-12 23:32:51 -0300

    feat(server): create error handler

---
+++ server/error.handler.ts
@@ -0,0 +1,21 @@
+import * as restify from 'restify'
+
+export const handleError = (req: restify.Request, res: restify.Response, error, done) => {
+
+  error.toJSON = () => {
+    return {
+      message: error.message
+    }
+  }
+  switch(error.name) {
+    case 'MongoError':
+      if(error.code === 11000) {
+        error.statusCode = 400
+      }
+      break
+    case 'ValidationError':
+      error.statusCode = 400
+    break
+  }
+  done()
+}
